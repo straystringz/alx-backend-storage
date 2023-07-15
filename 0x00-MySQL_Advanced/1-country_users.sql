901ac64e15ec52804575649a073817fb28668be0
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-07-15 17:56:13 +0000

    In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 --Script should be executable in any database
 
 CREATE TABLE `USERS` IF DOES NOT EXIXT (
-	`id` INT NOT NULL AUTO_INCREMENT,
+	`id` INT  AUTO_INCREMENT,
 	`email` VARCHAR (255) NOT NULL UNIQUE,
 	`name` VARCHAR (255),
 	`country` ENUM ( 'US', 'CO', 'TN') NOT NULL DEFAULT 'US',
