05d47ee58037d3d9b265b97c699151f90315ef81
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-07-15 16:35:35 +0000

    We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- Write a script that creates the table 'user'
 -- id should be int auto increment pri-key
-CREATE TABLE IF NOT EXISTS `user` (
+
+CREATE TABLE IF NOT EXISTS `users` (
 	`id` INT AUTO_INCREMENT PRIMARY KEY,
 	`email` VARCHAR (255) NOT NULL UNIQUE,
 	`name` VARCHAR (255)
