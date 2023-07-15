a76dbcaa50e9746c31468747ac8bca84d8920448
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-07-15 16:33:26 +0000

    We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,14 +1,5 @@
-/*
-Write a SQL script that creates a table users following these requirements
-With these attributes:
-id, integer, never null, auto increment and primary key
-email, string (255 characters), never null and unique
-name, string (255 characters)
-If the table already exists, your script should not fail
-Your script can be executed on any database
-*/
-
-
+-- Write a script that creates the table 'user'
+-- id should be int auto increment pri-key
 CREATE TABLE IF NOT EXISTS `user` (
 	`id` INT AUTO_INCREMENT PRIMARY KEY,
 	`email` VARCHAR (255) NOT NULL UNIQUE,
