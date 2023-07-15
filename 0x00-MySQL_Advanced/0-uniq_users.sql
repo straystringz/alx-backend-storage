56adaac68eac6d9ad4588feadce0ba970693ea5b
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-07-15 16:22:46 +0000

    We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -9,8 +9,8 @@ Your script can be executed on any database
 */
 
 
-CREATE TABLE IF NOT EXISTS user (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR (255) NOT NULL UNIQUE,
-	name VARCHAR (255)
+CREATE TABLE IF NOT EXISTS `user` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`email` VARCHAR (255) NOT NULL UNIQUE,
+	`name` VARCHAR (255)
 );
