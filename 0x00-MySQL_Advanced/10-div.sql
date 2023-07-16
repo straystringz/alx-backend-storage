64256cdc77c0982107a884d9b2628e35185d89ef
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-07-16 22:34:21 +0000

    Safe divide

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,18 +1,15 @@
--- SQL script that crts a stored procedure ComputeAverageWeightedScoreForUser
--- that computes and store the average weighted score fr a student.
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
-    user_id INT
-)
+-- SQL script that creates a func. SafeDiv that divides (and returns) the first by
+-- the 2nd number or returns 0 if the second number is equal to 0.
+DELIMITER |
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
 BEGIN
-    DECLARE w_avg_score FLOAT;
-    SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight) 
-                        FROM users AS U 
-                        JOIN corrections as C ON U.id=C.user_id 
-                        JOIN projects AS P ON C.project_id=P.id 
-                        WHERE U.id=user_id);
-    UPDATE users SET average_score = w_avg_score WHERE id=user_id;
-END
-$$
-DELIMITER ;
+    DECLARE result FLOAT;
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END;
