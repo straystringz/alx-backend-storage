1605743bb408020450df49997340eff8ba360747
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-07-16 15:03:06 +0000

    In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,10 @@
 --Same as 0-uniq_users.sql but, with country
 --Script should be executable in any database
 
-CREATE TABLE `users` IF DOES NOT EXIXT (
-	`id` INT  AUTO_INCREMENT,
+CREATE TABLE `users` IF DOES NOT EXISTS (
+	`id` INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
 	`email` VARCHAR (255) NOT NULL UNIQUE,
 	`name` VARCHAR (255),
-	`country` ENUM ( 'US', 'CO', 'TN') NOT NULL DEFAULT 'US',
-	PRIMARY KEY (id)
+	`country` ENUM ( 'US', 'CO', 'TN'),
+
 );
