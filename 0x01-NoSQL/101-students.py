28963481cb89bb7d1a6f05e93480700725a37696
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-07-23 16:58:54 -0700

    Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+students by sc
+"""
+
+
+def top_students(mongo_collection):
+    """ studnts by score """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
